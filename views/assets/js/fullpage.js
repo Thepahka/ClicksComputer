ee98a9eaf85b7067c8777243de3076463db1b0f7
jancock <yausuga48@misena.edu.co>
2017-11-23 06:48:27 -0500

    2.4 commit

--- views/assets/js/fullpage.js
+++ views/assets/js/fullpage.js
@@ -7,15 +7,9 @@ $('#fullpage').fullpage({
   navigation: true,
   slidesNavigation: true,
   controlArrows: false,
-  anchors: ['firstSection', 'secondSection', 'thirdSection'],
   menu: '#menu',
 
-  afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
-    if(anchorLink == 'fifthSection' && slideIndex == 1) {
+  afterSlideLoad: function( anchorLink, index,slideIndex) {
       $.fn.fullpage.setAllowScrolling(false, 'up');
-      $header_top.css('background', 'transparent');
-      $nav.css('background', 'transparent');
-      $(this).css('background', '#ff0000');
-    }
   },
 });
