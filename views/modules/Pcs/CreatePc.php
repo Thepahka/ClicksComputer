fa4802dabfe6d806dfa3555172ef2fe99e95a06a
jancock <yausuga48@misena.edu.co>
2017-12-11 17:14:04 -0500

    2.20 commit

--- views/modules/Pcs/CreatePc.php
+++ views/modules/Pcs/CreatePc.php
@@ -5,31 +5,36 @@
     <link rel="shortcut icon" type="image/x-icon" href="views/assets/image/logo.png">
     <title>Crear Pc</title>
     <header>
-      <h1>Registrar Computador</h1>
     </header>
   </head>
-  <body>
+  <body class="body-dash">
+    <div class="wrap-nav">
+    <h1 class="titulo-newpc">Registrar Computador</h1>
     <form class="wrap-pcnew" action="Registro-Completado" method="post">
-        <label for="CreateId">Codigo del computador</label>
-        <input id="CreateId" type="text" name="data[]"></br>
-        <label for="CreateName">Nombre del computador</label>
-        <input id="CreateName" type="text" name="data[]"></br>
-        <label for="CreateDesc">Descripcion del computador</label>
-        <textarea id="CreateDesc" name="data[]"></textarea></br>
-        <label for="CreateMod">Modelo del computador</label>
-        <input id="CreateMod" type="text" name="data[]"></br>
-        <label for="CreateTip">Tipo de computador</label>
-        <input id="CreateTip" type="text" name="data[]"></br>
-        <label for="CreateFicha" class="pdf">Ficha tecnica del computador</label>
-        <input id="CreateFicha" type="file" name="data[]"></br>
-        <label for="CreateMarca">Marca del computador</label>
-        <input id="CreateMarca" type="text" name="data[]"></br>
-        <label for="CreatePrecio">Precio del computador</label>
-        <input id="CreatePrecio" type="text" name="data[]"></br>
-        <input id="CreateEnter" type="submit" value="Registrar">
+        <label for="CreateId" class="cpc">Codigo del computador</label>
+        <input id="CreateId" type="text" name="data[]" class="in-cpcod"></br>
+        <label for="CreateName" class="cpc">Nombre del computador</label>
+        <input id="CreateName" type="text" name="data[]" class="in-cpcname"></br>
+        <label for="CreateDesc" class="cpc desc">Descripcion del computador</label>
+        <textarea id="CreateDesc" name="data[]" class="in-cpcarea"></textarea></br>
+        <label for="CreateMod" class="cpc">Modelo del computador</label>
+        <input id="CreateMod" type="text" name="data[]" class="in-cpcmodel"></br>
+        <label for="CreateTip" class="cpc">Tipo de computador</label>
+        <input id="CreateTip" type="text" name="data[]" class="in-cpctipo"></br>
+        <label for="CreateFicha" class="pdf cpc">Ficha tecnica del computador</label>
+        <input id="CreateFicha" type="file" name="data[]" class="in-cpcficha custom-file-input"></br>
+        <label for="CreateMarca" class="cpc">Marca del computador</label>
+        <input id="CreateMarca" type="text" name="data[]" class="in-cpcmarca"></br>
+        <label for="CreatePrecio" class="cpc">Precio del computador</label>
+        <input id="CreatePrecio" type="text" name="data[]" class="in-cpcprecio"></br>
+        <input id="CreateEnter" type="submit" class="btn-cpc" value="Registrar">
+      </div>
     </form>
 
     <?php require_once 'views/include/scope.menutopdashboard.php';
           require_once 'views/include/scope.profileenterprise.php';?>
+
+  <script type="text/javascript" src="views/assets/js/unputfile.js"></script>
+
   </body>
 </html>
